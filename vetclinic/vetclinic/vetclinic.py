47b4798206f9aa8a27b43ceb931db26985bc3341
crazyshare <anorange7@hotmail.com>
2015-12-21 14:05:20 +0800

    Initial commit

---
+++ vetclinic/vetclinic/vetclinic.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from openerp.osv import osv, fields
+
+class vetclinic_animal(osv.Model):
+    _name ="vetclinic.animal"
+    _columns = {
+        'name': fields.char('Name', size=64),
+        'birthdate': fields.date('Birth Date'),
+    }
+    
+##vetclinic_animal() ##optinal in version 7
+      
+class vetclinic_classification(osv.Model):
+    _name = "vetclinic.classification"
+    _columns = {
+        'name': fields.char('Name', size=32),
+    }
+
+class vetclinic_breed(osv.Model):
+    _name = "vetclinic.breed"
+    _columns = {
+        'name': fields.char('Name', size=32),
+    }
