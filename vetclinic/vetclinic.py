4ebcb5777f04a53214e7e4845d0010e2bf2da87b
youdianlian <anorange7@hotmail.com>
2015-12-22 11:30:58 +0800

    删除多余的文件

--- vetclinic/vetclinic.py
+++ vetclinic/vetclinic.py
@@ -2,10 +2,24 @@
 from openerp.osv import osv, fields
 
 class vetclinic_animal(osv.Model):
-    _name_="vetclinic_animal"
+    _name    = "vetclinic.animal"
     _columns = {
-        'name': fields.char('Name', size=64),
-        'birthdate':fields.date('Birth Date')
+        'name'             : fields.char('Name', size=64),
+        'birthdate'        : fields.date('Birth Date'),
+        'classification_id': fields.many2one('vetclinic.classification','Classification'),
+        'breed_id'         : fields.many2one('vetclinic.breed','Breed'),
     }
     
 ##vetclinic_animal() ##optinal in version 7
+      
+class vetclinic_classification(osv.Model):
+    _name    = "vetclinic.classification"
+    _columns = {
+        'name': fields.char('Name', size=32),
+    }
+
+class vetclinic_breed(osv.Model):
+    _name    = "vetclinic.breed"
+    _columns = {
+        'name': fields.char('Name', size=32),
+    } 
